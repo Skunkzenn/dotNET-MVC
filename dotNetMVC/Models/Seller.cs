571aa96e995acca5f069b209ecbdfd085fe3a22e
Victor Destefani <a18586@alunos.ipca.pt>
2024-08-25 21:50:09 +0100

    App locale, number and date formatting

--- dotNetMVC/Models/Seller.cs
+++ dotNetMVC/Models/Seller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace dotNetMVC.Models
@@ -8,10 +9,22 @@ namespace dotNetMVC.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        //Altera nome do objeto visualmente no display(front-end)
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+
+        [Display(Name = "Base Salary")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; } //Associação ao departamento
+
+        [Display(Name = "Department")]
         public int DepartmentId { get; set; } //Inclui o ID do departamento, indicando para o entity framework que o ID terá que existir na classe seller,
                                               //garantindo assim que o ID não será nulo. (tipo int não pode ser nulo!)
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>(); //Associação sob o SalesRecord, INSTÂNCIAR!!
--- dotNetMVC/Startup.cs
+++ dotNetMVC/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
 using dotNetMVC.Data;
 using dotNetMVC.Services;
@@ -58,6 +60,17 @@ namespace dotNetMVC
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
+            //definindo o locale da aplicação com formatato dos USA
+            var enUS = new CultureInfo("en-US");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { enUS }
+            };
+
+            app.UseRequestLocalization(localizationOptions);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
