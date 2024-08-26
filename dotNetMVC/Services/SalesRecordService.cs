0c250b51f524cf94182bb34c5978140cd15a6e9c
Victor Destefani <a18586@alunos.ipca.pt>
2024-08-26 20:46:22 +0100

    Implementing grouping search

--- dotNetMVC/Services/SalesRecordService.cs
+++ dotNetMVC/Services/SalesRecordService.cs
@@ -39,5 +39,27 @@ namespace dotNetMVC.Services
                 .OrderByDescending(x => x.Date)
                 .ToListAsync();
         }
+
+        public async Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            //Pega no SalesRecord que é do tipo DbSet e construir um obj do tipo result IQueryable
+            //E assim, em cima desse objeto podemos acrescentar detalhes sobre a consulta
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .GroupBy(x => x.Seller.Department) // Agrupa pelo departamento, Task<List<IGrouping<Department, SalesRecord>>>
+                                                   // Quando se agrupa resultados, o tipo de retorno não será mais uma lista e sim os resultados ficam em uma coleção chamada IGrouping
+                .ToListAsync();
+        }
     }
 }
